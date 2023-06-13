3bd8e4fff1a8bbbeab2931175dd884da8aad4852
abraham <55025669+medium-technica@users.noreply.github.com>
2023-06-13 12:21:41 +0530

    unified index.html test

--- static/theme-mdl.js
+++ static/theme-mdl.js
@@ -1,5 +1,6 @@
 $(fnInit())
 
+var Title;
 var ListFiles = [];
 var ListTitleFiles = [];
 var Articles;
@@ -7,8 +8,28 @@ var Description;
 
 function fnInit() {
  console.log("init");
- fnLoadData();
+ fnLoadInit();
+ //fnLoadData();
 }
+
+function fnLoadInit() {
+ fetch("static/init.json")
+  .then(response => response.json())
+  .then(data => {
+   console.log(data["description"]);
+   Title = data["title"];
+   Description = data["description"];
+   document.querySelector("title").innerHTML = Title;
+   document.querySelector(".mdl-layout-title").innerHTML = Title + '<div class="title-description">' + Description + '</div>';
+   document.querySelector("#menu-item-right").innerHTML = data["title-menu-right"];
+   document.querySelector("#menu-item-left").innerHTML = data["title-menu-left"];
+   document.querySelector("body").setAttribute("style", "font-family:" + data["body-style-font-family"])
+   document.querySelector('meta[property="og:title"]').setAttribute("content", Title);
+   document.querySelector('meta[property="og:description"]').setAttribute("content", Description);
+   fnLoadData();
+  });
+}
+
 function fnLoadData() {
  fetch("static/res/doc/text.json")
   .then(response => response.json())
